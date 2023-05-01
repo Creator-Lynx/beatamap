2b0ba3dbbdc3b4b942b6c5b60d51a3ab8f7822fb
BorisDombrowski <32637303+BorisDombrowski@users.noreply.github.com>
2023-05-01 17:55:56 +1000

    skeleton enemy

--- Assets/Gameplay/Scripts/PartsExplosion.cs
+++ Assets/Gameplay/Scripts/PartsExplosion.cs
@@ -11,7 +11,7 @@ public class PartsExplosion : MonoBehaviour
         foreach (var part in _parts) 
         {                       
             //part.AddForceAtPosition(hitDirection, hitPoint, ForceMode.Impulse);
-            part.AddForce(hitDirection * 2f, ForceMode.Impulse);
+            part.AddForce(hitDirection * 7f, ForceMode.Impulse);
         }
     }
 }
